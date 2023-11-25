067326ad62f7169818daccdd575f80ff693e6d02
s-hirano-ist <88515957+s-hirano-ist@users.noreply.github.com>
2023-11-25 10:39:10 +0900

    feat: introduce passport auth (#187)
    * chore: downgrade prisma
    
    * add basic and digest auth
    
    * change to basic auth

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express from "express";
+import passport from "passport";
+import passportHttp from "passport-http";
 import swaggerUi from "swagger-ui-express";
 import { RegisterRoutes } from "../build/routes";
 import swaggerDocument from "../build/swagger.json";
@@ -9,6 +11,23 @@ import "dotenv/config";
 const app = express();
 
 const PORT = process.env.PORT ?? 8080;
+
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+
+if (!USERNAME || !PASSWORD) throw new Error("secrets missing.");
+passport.use(
+  new passportHttp.BasicStrategy((username, password, done) => {
+    if (username === USERNAME && password === PASSWORD) return done(null, true);
+    else return done(null, false);
+  }),
+);
+// passport.use(
+//   new passportHttp.DigestStrategy({ qop: "auth" }, (username, cb) => {
+//     if (username === USERNAME) return cb(null, username, PASSWORD);
+//     else return cb(null, false);
+//   }),
+// );
 
 app.use(
   cors({
@@ -20,6 +39,14 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  passport.authenticate("basic", {
+    session: false,
+  }),
+  // passport.authenticate("digest", {
+  //   session: false,
+  // }),
+);
 // this route file is not included in Swagger UI
 app.use("/v1/news-detail", newsDetailRoutes);
 
